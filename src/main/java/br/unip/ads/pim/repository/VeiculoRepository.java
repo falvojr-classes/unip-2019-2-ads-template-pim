4c51a93f6ff95aedef21f6be45e6da0e6481352a
Venilton FalvoJr <falvojr@gmail.com>
2019-11-04 22:55:09 -0200

    Aula 4/11

--- src/main/java/br/unip/ads/pim/repository/VeiculoRepository.java
+++ src/main/java/br/unip/ads/pim/repository/VeiculoRepository.java
@@ -9,4 +9,12 @@ public interface VeiculoRepository extends CrudRepository<Veiculo, Long> {
 
 	@Query("FROM Veiculo v WHERE v.inativo = false")
 	Iterable<Veiculo> findActives();
+	
+	// TODO Cria a query abaixo (ativos e disponíveis):
+	//SELECT DISTINCT v.* FROM veiculo v 
+	//WHERE v.inativo = 0 AND
+	//	(SELECT COUNT(*) 
+	//	FROM ocorrencia o 
+	//    WHERE o.veiculo_id = v.id 
+	//    AND o.fim IS NULL) = 0
 }
